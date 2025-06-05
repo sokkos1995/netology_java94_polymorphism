390f2e1dd7b8951593d6c03a85a3214bc821847f
k.sokolov1 <k.sokolov@netology.tech>
2025-06-05 09:32:47 +0300

    fix sale

--- deals/Expenditure.java
+++ deals/Expenditure.java
@@ -2,13 +2,9 @@ package deals;
 
 public class Expenditure extends Deal {
     public Expenditure(int debitChange, int creditChange, String comment) {
-        super(debitChange, creditChange, comment);
+        super(0, creditChange, "Покупка " + comment + " на " + creditChange + " руб.");
         if (creditChange <= 0) {
             throw new IllegalArgumentException("Сумма покупки не может быть меньше или равна нулю");
-        }
-        if (debitChange != 0) {
-            throw new IllegalArgumentException("При покупке сумма доходов должна быть равна нулю");
-        }               
-        // this.comment = "Покупка " + comment + " на " + creditChange + " руб.";
+        }             
     }    
 }
--- deals/Sale.java
+++ deals/Sale.java
@@ -2,14 +2,9 @@ package deals;
 
 public class Sale extends Deal {
     public Sale(int debitChange, int creditChange, String comment) {
-        super(debitChange, creditChange, comment);
+        super(debitChange, 0, "Продажа " + comment + " на " + debitChange + " руб.");
         if (debitChange <= 0) {
             throw new IllegalArgumentException("Сумма продажи не может быть меньше или равна нулю");
-        }
-        if (creditChange != 0) {
-            throw new IllegalArgumentException("При продаже сумма расходов должна быть равна нулю");
-        }        
-        // this.comment = "Продажа " + comment + " на " + debitChange + " руб.";
-
+        } 
     }    
 }
